17cb8f96ef9d6d808b6fe34947c939a45c866dc2
Monika-feg <mala88@hotmail.comcd>
2025-09-24 16:30:53 +0200

    lagt till lite ändringar i cssen och fixat några stav fel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="home-container">
       <header>
         <p>Välkommen till min digitala CV-sida!</p>
         <h1>Monika Engström - Fullstackutvecklare</h1>
